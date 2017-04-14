de56b258e580c671527420884f2d42dfc5f067a9
Dejan <ddinic2@gmail.com>
2017-04-14 20:58:08 +0200

    First version

---
+++ main.js
@@ -0,0 +1,10 @@
+var openMenu = function() {
+    if ($('.nav_ul').hasClass('open')) {
+        $('.nav_ul').removeClass('open');
+        $('#menuLink').getElementById("menuLink");
+    }
+    else {
+        $('.nav_ul').addClass('open');
+        $('#menuLink').getElementById("menuLink");
+    }
+};
